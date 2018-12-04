67db2f2df2e96f7b0eb0355c76a1f3ded3c20e89
remidev <28935481+remidev@users.noreply.github.com>
2018-12-03 21:44:23 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Get DOM Elements
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const menuBranding = document.querySelector(".menu-branding");
+
+// Set Inital State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menu.classList.add("show");
+    menuBtn.classList.add("close");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menu.classList.remove("show");
+    menuBtn.classList.remove("close");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
